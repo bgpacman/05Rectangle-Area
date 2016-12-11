95a7ad54b97a88c06c9980dec83b5a07dfeffbbd
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-12-11 18:21:59 +0200

    05Rectangle Area
    05Rectangle Area

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Length Of A Rectangle:");
+            var a = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Width Of A Rectangle:");
+            var b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Rectangle Area is");
+            var area = (a * b);
+            Console.WriteLine(area);
+        }
+    }
+}
